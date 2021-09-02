9582db4901c091148d9a90f02cf66ab9705f8ef9
Voo Yee Xing <vyxing27@gmail.com>
2021-09-02 10:51:05 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
+const API_key = "325dcc0b72cdfc17143d7d6556c5a967";
+const endpoint = "https://api.openweathermap.org/data/2.5/weather?";
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  const fetchAPI = async () => {
+    const url = endpoint + "q=London&appid=" + API_key;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log("This is the response.", data);
+
+      setLoading(false);
+    } catch (error) {
+      console.log("API Fetching Error.", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
